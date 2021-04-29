0f2275fd9742641b4c33cc4aa8014eb3ba747ba8
GitHub Action <action@github.com>
2021-04-29 07:11:20 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/zxbjob/myintellijgitplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.zxbjob.myintellijgitplugin.services
+
+import com.github.zxbjob.myintellijgitplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/zxbjob/myintellijgitplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/zxbjob/myintellijgitplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.zxbjob.myintellijgitplugin.services
 
+import com.github.zxbjob.myintellijgitplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
